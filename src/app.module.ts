f8bf7a365243f4bf4544060ba701271a8d79ff14
Jeaninny Teixeira <jeaninny.teixeira@gmail.com>
2026-03-12 13:21:37 -0300

    🐛 FIX: Registro de VeiculoModule em app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { VeiculoModule } from './veiculo/veiculo.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: false,
       logging: false,
-    })
+    }),
+    VeiculoModule    
   ],
   controllers: [],
   providers: [],
