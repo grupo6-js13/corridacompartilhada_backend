adcd984dcf2df1f4abc4dde1a4479d64bc56184a
mwendell <mwendellsmce@gmail.com>
2026-04-16 08:35:26 -0300

    Adicionando documentacao swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00'; // Fuso horário de Brasília
+  process.env.TZ = '-03:00';
+
+  // Configurações do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Corridas Compartilhadas')
+    .setDescription('API REST do projeto de caronas universitárias da Orbyte(Grupo 6)')
+    .setContact("Grupo 6 JS13", "https://github.com/grupo6js13", "")
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document); // Define a rota da documentação
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors(); // Essencial para o React conseguir consumir a API
+  app.enableCors();
 
-  // Usa a porta do Render ou a 4000 localmente
-  await app.listen(process.env.PORT ?? 4000); 
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
